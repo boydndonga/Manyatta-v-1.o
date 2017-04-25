35d54fc2b3c91747653003933afa41cde12a72af
kahu08 <peterkahurani@gmail.com>
2017-04-25 16:12:17 +0300

    added model relationship for users(houses,reviews and bookings)

--- app/models/users.js
+++ app/models/users.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  avatar: DS.attr('string'),
+  occupation: DS.attr('string'),
+  identification: DS.attr('string'),
+
+  houses: DS.hasMany('house'),
+  reviews: DS.hasMany('review'),
+  bookings: DS.hasMany('booking')
 
 });
