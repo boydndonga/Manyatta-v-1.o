c65f404439df555dd7c591870be7976a4ff1de67
kahu08 <peterkahurani@gmail.com>
2017-04-25 16:31:55 +0300

    added model relationship for houses(users,bookings and reviews)

--- app/models/houses.js
+++ app/models/houses.js
@@ -1,5 +1,19 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  /*defining attributes of houses and their relationship*/
+
+  house - name: DS.attr('string'),
+  house - description: DS.attr('string'),
+  tel - number: DS.attr('number'),
+  city: DS.attr('string'),
+  street: DS.attr('string'),
+  rules: DS.attr('string'),
+  ammenities: DS.attr('string'),
+  price: DS.attr('string'),
+
+  reviews: DS.hasMany('review'),
+  bookings: DS.hasMany('booking'),
+  users: DS.belongsTo('user')
 
 });
