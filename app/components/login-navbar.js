90ba6bff0708e5e9fb825812e3266ab1a1d31e31
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-04-25 16:27:09 +0300

    got the email and password on sign up and sent action to application.js

--- app/components/login-navbar.js
+++ app/components/login-navbar.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions:{
+    signUp(){
+      var email=this.get("email");
+      var password=this.get("password");
+
+      this.sendAction("signUpCreds", email,password);
+    }
+  }
 });
