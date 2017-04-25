d4afa566cdcf33720d1b40acb1332fc473abe390
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-04-25 16:32:48 +0300

    added logic to application.js to add logic to add a new user to firebase

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   beforeModel:function(){
     return this.get('session').fetch().catch(function(){});
   },
+  actions:{
+    signUp(email,password){
+      this.get('session').open('firebase',{
+        provider:'password',
+        email:email,
+        password:password
+      });
+    }
+  }
 });
