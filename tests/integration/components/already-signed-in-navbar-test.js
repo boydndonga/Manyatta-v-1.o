165c9a23e18b53b4b8bd6c716bb956c9c98ceceb
kahu08 <peterkahurani@gmail.com>
2017-04-26 10:29:28 +0300

    ALREADY-SIGNED-IN NAVBAR 1.Deleted the sign-in navbar component with its dependancies 2.created an already signed in navbar with all its dependancies 3.added html code previously in sign-in component and commented  what it does

--- tests/integration/components/already-signed-in-navbar-test.js
+++ tests/integration/components/already-signed-in-navbar-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('sign-in-navbar', 'Integration | Component | sign in navbar', {
+moduleForComponent('already-signed-in-navbar', 'Integration | Component | already signed in navbar', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{sign-in-navbar}}`);
+  this.render(hbs`{{already-signed-in-navbar}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#sign-in-navbar}}
+    {{#already-signed-in-navbar}}
       template block text
-    {{/sign-in-navbar}}
+    {{/already-signed-in-navbar}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
